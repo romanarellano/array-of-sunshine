a51497abb5ae88b28a8d5d12e9fef708a397d381
romanarellano <romanarellano@yahoo.com>
2015-01-15 18:45:31 -1000

    used splice two times

--- sunshine.js
+++ sunshine.js
@@ -17,31 +17,31 @@ return revStr;
   
 }
 
-// /**
-//   Write a function that takes two parameters as input in the order
-//   of string, character. With the first string parameter use Array
-//   functionality remove the last character and put it at the beginning
-//   of the array. Then insert the character passed in the second
-//   parameter into the second to last position of the array.
-//   Then convert the array to a string and return it.
-// */
-// function spliceString(str, char) {
-//       var arr= str.split("");
-//      arr.splice(0,0,arr[arr.length-1]);
-//      arr.splice(-2,0,char); 
-//   var string=arr.join("");
-// var condense= string.substring(0,string.length-1);
-//   return condense;
+/**
+  Write a function that takes two parameters as input in the order
+  of string, character. With the first string parameter use Array
+  functionality remove the last character and put it at the beginning
+  of the array. Then insert the character passed in the second
+  parameter into the second to last position of the array.
+  Then convert the array to a string and return it.
+*/
+function spliceString(str, char) {
+      var arr= str.split("");
+     arr.splice(0,0,arr[arr.length-1]);
+     arr.splice(-2,0,char); 
+  var string=arr.join("");
+var condense= string.substring(0,string.length-1);
+  return condense;
 
-// }
+}
 
-// /**
-//   Using the Array map() method write a function that takes an array
-//   of strings as input and returns a duplicate array with all strings
-//   in pig latin form. (Ex. "metal" => "etalmay".)
-//   Note: For now, do not worry about words like "chicken".
-//   Instead of "ickenchay", it should just become "hickencay".
-// */
+/**
+  Using the Array map() method write a function that takes an array
+  of strings as input and returns a duplicate array with all strings
+  in pig latin form. (Ex. "metal" => "etalmay".)
+  Note: For now, do not worry about words like "chicken".
+  Instead of "ickenchay", it should just become "hickencay".
+*/
 // function speakPigLatin(strArray) {
 
 // var array=[];
